5f29622e3c29310440df9ad427b02fd196838819
simke <simke.nys@gmail.com>
2024-01-09 13:04:11 +0100

    0. Start

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import { Title } from './Title/Title';
+
+export const Dashboard = (props) => {
+  return (
+    <div style={{ width: '100%' }}>
+      <h1 style={{ textAlign: 'center' }}>{value}</h1>;
+    </div>
+  );
+};
---
+++ src/Components/Dashboard/Title/Title.jsx
@@ -0,0 +1,4 @@
+export const Title = (props) => {
+  const { value } = props;
+  return <h1 style={{ textAlign: 'center' }}>{value}</h1>;
+};
