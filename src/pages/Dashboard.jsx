0c2ff4fd01a7b0673d95cad0cf7b64406671b17d
simke <simke.nys@gmail.com>
2024-01-09 14:11:51 +0100

    Setup grid

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Title } from '../components/Title/Title';
+import { Box, Grid, Paper } from '@mui/material';
 
 export const Dashboard = (props) => {
   return (
-    <div style={{ width: '100%' }}>
+    <Box sx={{ width: '100%', p: 2 }}>
       <Title value="My title (click me)"></Title>
-    </div>
+
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Paper sx={{ p: 2 }}>Bars</Paper>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper sx={{ p: 2 }}>Lines</Paper>
+        </Grid>
+        <Grid item xs={12} md={12}>
+          <Paper sx={{ p: 2 }}>Table</Paper>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
